208e8bec001f13e4100fafcc5e9f9a9ba4e8934d
FRY Developer <alexiusandromedavsgalaxia@gmail.com>
2026-09-25 11:50:46 +0200

    system: add filesystem kernel and boot layers

---
+++ src/system/boot.js
@@ -0,0 +1,2 @@
+export const BOOT_STAGES=["Firmware","Bootloader","Kernel","Userspace","Desktop"];
+export const BOOT_LABELS={Firmware:"Checking virtual hardware",Bootloader:"Loading /System/Boot/boot.efi",Kernel:"Starting makKernel",Userspace:"Starting system services",Desktop:"Launching makOS Tahoe"};
